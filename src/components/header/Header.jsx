c094b3e2dbe1a6b2f8884ef8b4f0688ab7cc294b
Pratham Matkar <prathammatkar2506@gmail.com>
2024-08-01 17:03:23 +0530

    header updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5 className='aligntextcentermain'>Hello I'm</h5>
         <h1>Pratham Matkar</h1>
-        <h5 className="text-light">Digging Data</h5>
+        <h5 className="text-light">VESIT '26 | Web Dev Intern @ Rhyno EV | Jr. Web Editor @ IEEE - VESIT <br />Ex R & D Intern @ Indrones | GPM '23</h5>
         <CTA/>
         <Headersocial/>
 
